aa9af199489332bc2db16334482dc17b777fd6bf
afrozarahmanp <afroza.rahman.p@gmail.com>
2023-09-05 19:19:56 +0600

    Firebase config file edited

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBUZY5jwmHUSxUZ6mmP7CtxGLQGAidGEhM",
-  authDomain: "animetoyemporium.firebaseapp.com",
-  projectId: "animetoyemporium",
-  storageBucket: "animetoyemporium.appspot.com",
-  messagingSenderId: "157605824066",
-  appId: "1:157605824066:web:c12161718843b7817f0fdb"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
